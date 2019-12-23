25bb949df9eef3cf375bca9f8943a22073426df0
Senpai <belavalmax@sfr.fr>
2019-12-23 19:34:37 +0000

    Add for 'Module NRF24L01' : Codes : Tx_01 and Rx_01

--- NRF24L01/TEST_NRF24L01_RX_01.c
+++ NRF24L01/TEST_NRF24L01_RX_01.c
@@ -1,6 +1,84 @@
 /*
-Flying Diamond Project - Test Repository
-Purpose		:	Test code for the NRF24L01 module (Rx only)
-File name	:	TEST_NRF24L01_RX_01.c
+ * Flying Diamond Project - Test Repository
+ * Purpose			:	Test code for the NRF24L01 module (Rx only)
+ * File name		:	TEST_NRF24L01_RX_01.c
+ */
+
+/* Receiver code for the Arduino Radio control with PWM output.
+ * 
+ * Requirements : Install the NRF24 library to your IDE.
+ *
+ * This code has been written to be uploaded on the following targets : Arduino UNO, NANO, Pro mini (5V,16MHz).
+ *
+ * Connect a NRF24 module to it :
+ 
+    Module // Arduino UNO,NANO
+    
+    GND    ->   GND
+    Vcc    ->   3.3V
+    CE     ->   D9
+    CSN    ->   D10
+    CLK    ->   D13
+    MOSI   ->   D11
+    MISO   ->   D12
+
+This code receive 1 channels and prints the value on the serial monitor.
 */
 
+/**************************************************/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+const uint64_t pipeIn = 0xE8E8F0F0E1LL;		// This ID code must be the same as in the transmitter.
+RF24 radio(9, 10);							// CSN and CE pins.
+
+// The sizeof this struct should not exceed 32 bytes (max size of data that can be send through the NRF24L01 module).
+struct Received_data {
+	byte ch1;
+};
+
+int ch1_value = 0;
+Received_data received_data;
+
+/**************************************************/
+
+void setup() {
+	Serial.begin(9600);
+
+	// We reset the received values.
+	received_data.ch1 = 127;
+ 
+	// Once again, begin and radio configuration.
+	radio.begin();
+	radio.setAutoAck(false);
+	radio.setDataRate(RF24_250KBPS);  
+	radio.openReadingPipe(1,pipeIn);
+  
+	// We start the radio comunication.
+	radio.startListening();
+}
+
+/**************************************************/
+
+unsigned long last_Time = 0;
+
+// We create the function that will read the data each certain time.
+void receive_the_data() {
+	while ( radio.available() )
+	{
+		radio.read(&received_data, sizeof(Received_data));
+		last_Time = millis();	// Here we receive the data.
+	}
+}
+
+/**************************************************/
+
+void loop() {
+	// Receive the radio data.
+	receive_the_data();
+
+	ch1_value = map(received_data.ch1,0,255,1000,2000);
+	Serial.println(ch1_value);
+}
--- NRF24L01/TEST_NRF24L01_TX_01.c
+++ NRF24L01/TEST_NRF24L01_TX_01.c
@@ -1,6 +1,62 @@
 /*
-Flying Diamond Project - Test Repository
-Purpose		:	Test code for the NRF24L01 module (Tx only)
-File name	:	TEST_NRF24L01_TX_01.c
+ * Flying Diamond Project - Test Repository
+ * Purpose			:	Test code for the NRF24L01 module (Tx only)
+ * File name		:	TEST_NRF24L01_TX_01.c
+ */
+
+/* Tranmsitter code for the Arduino Radio control with PWM output.
+ *
+ * Requirements : Install the NRF24 library to your IDE.
+ *
+ * This code has been written to be uploaded on the following targets : Arduino UNO, NANO, Pro mini (5V,16MHz).
+ *
+ * Connect a NRF24 module to it :
+ 
+    Module // Arduino UNO,NANO
+    
+    GND    ->   GND
+    Vcc    ->   3.3V
+    CE     ->   D9
+    CSN    ->   D10
+    CLK    ->   D13
+    MOSI   ->   D11
+    MISO   ->   D12
+
+This code transmits 1 channels with data from pins A0 POTENTIOMETER.
 */
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+const uint64_t my_radio_pipe = 0xE8E8F0F0E1LL;		// This ID code must be the same as in the transmitter.
+RF24 radio(9, 10);									// CSN and CE pins.
+
+// The sizeof this struct should not exceed 32 bytes (max size of data that can be send through the NRF24L01 module).
+struct Data_to_be_sent {
+	byte ch1; 
+};
+
+Data_to_be_sent sent_data;
+
+/**************************************************/
+
+void setup() {
+	radio.begin();
+	radio.setAutoAck(false);
+	radio.setDataRate(RF24_250KBPS);
+	radio.openWritingPipe(my_radio_pipe);  
+	sent_data.ch1 = 127;
+}
+
+/**************************************************/
+
+void loop() {
+	/* If your channel is reversed, just swap 0 to 255 by 255 to 0 below
+	EXAMPLE:
+	Normal:    data.ch1 = map( analogRead(A0), 0, 1024, 0, 255);
+	Reversed:  data.ch1 = map( analogRead(A0), 0, 1024, 255, 0);  */
+	
+	sent_data.ch1 = map( analogRead(A0), 0, 1024, 0, 255);
+	radio.write(&sent_data, sizeof(Data_to_be_sent));
+}
