48e3f276cabb07c153a9613c3f6848bc363c0d3c
Senpai <belavalmax@sfr.fr>
2019-12-25 18:23:37 +0000

    Update files

---
+++ NRF24L01/TEST_NRF24L01_TX_02--NOT-FINISHED.c
@@ -0,0 +1,59 @@
+/*
+ * Flying Diamond Project - Test Repository
+ * Purpose			:	Test code for the NRF24L01 module (Tx only)
+ * File name		:	TEST_NRF24L01_TX_01.c
+ */
+
+/* Tranmsitter code for the Arduino Radio control with PWM output.
+ *
+ * Requirements : Install the NRF24 library to your IDE.
+ *
+ * This code has been written to be uploaded on the following targets : Arduino UNO, NANO, Pro mini (5V,16MHz).
+ *
+ * Connect a NRF24 module to it :
+ 
+    Module // Arduino UNO,NANO
+    
+    GND    ->   GND
+    Vcc    ->   3.3V
+    CE     ->   D9
+    CSN    ->   D10
+    MOSI   ->   D11
+    MISO   ->   D12
+    CLK    ->   D13
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+const uint64_t my_radio_pipe = 0xE8E8F0F0E1LL;		// This ID code must be the same as in the transmitter.
+RF24 radio(9, 10);									// CSN and CE pins.
+
+// The sizeof this struct should not exceed 32 bytes (max size of data that can be send through the NRF24L01 module).
+struct Data_to_be_sent {
+	byte data; 
+};
+
+Data_to_be_sent sent_data;
+
+/**************************************************/
+
+void setup() {
+	radio.begin();
+	radio.setAutoAck(false);
+	radio.setDataRate(RF24_250KBPS);
+	radio.openWritingPipe(my_radio_pipe);  
+	sent_data.data = 127;
+}
+
+/**************************************************/
+
+void loop() {
+	/* If your channel is reversed, just swap 0 to 255 by 255 to 0 below
+	Normal:    data.ch1 = map( analogRead(A0), 0, 1024, 0, 255);
+	Reversed:  data.ch1 = map( analogRead(A0), 0, 1024, 255, 0);  */
+	
+	sent_data.data = map( analogRead(A0), 0, 1024, 0, 255);	// map : data, input min, input max, output min, output max
+	radio.write(&sent_data, sizeof(Data_to_be_sent));
+}
